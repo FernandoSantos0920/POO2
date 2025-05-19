3e3f878952508141e79d0894a2bc304339a29f63
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2025-05-19 16:32:10 -0300

    Colocar animações

--- PooExemplo1.2/Assets/Scripts/Player.cs
+++ PooExemplo1.2/Assets/Scripts/Player.cs
@@ -5,35 +5,49 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
 
     public float velocidade = 10f;
+
+    private Animator animator;
+    
+    
+    
+    
     void Start()
+   
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
-   
+    
     void Update()
     {
+      
+        
         
         Vector3 position = transform.position;
 
         if (Input.GetKey(KeyCode.A))
         {
             position.x = position.x - velocidade * Time.deltaTime;
+           
         }
         
         if (Input.GetKey(KeyCode.D))
         {
             position.x = position.x + velocidade * Time.deltaTime;
+            animator.SetBool("Movendo", true);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             position.z = position.z - velocidade * Time.deltaTime;
+           
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             position.z = position.z + velocidade * Time.deltaTime;
+          
         }
         
         transform.position = position;
